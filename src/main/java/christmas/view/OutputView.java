702d81605525f6b59c1ba1657b893e2a2ddac4b0
jongmee <ra123221@gmail.com>
2023-11-10 15:46:30 +0900

    feat(OutputView): 프로그램에서 출력할 때 사용하는 메세지들 view에서 private 상수로 관리하기

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,11 @@
+package christmas.view;
+
+public class OutputView {
+    private static final String ORDER_MENU_HEADER = "<주문 메뉴>";
+    private static final String ORDER_AMOUNT_HEADER = "<할인 전 총주문 금액>";
+    private static final String GIFT_MENU_HEADER = "<증정 메뉴>";
+    private static final String BENEFIT_HEADER = "<혜택 내역>";
+    private static final String BENEFIT_AMOUNT_HEADER = "<총혜택 금액>";
+    private static final String TOTAL_PAYMENT_HEADER = "<할인 후 예상 결제 금액>";
+    private static final String BADGE_HEADER = "<12월 이벤트 배지>";
+}
