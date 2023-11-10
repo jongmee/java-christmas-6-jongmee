75555c2a41f1b75dad3dfcd89b137353c7209b02
jongmee <ra123221@gmail.com>
2023-11-10 21:32:41 +0900

    feat(VisitDate): 방문 날짜에 대한 도메인 클래스 생성하기
    - WHY: VisitDateRequest 객체 내부에서 VisitDate를 생성하여 객체 생성을 캡슐화하였다. 또한 date 속성은 null 값을 암시할 필요가 없고 보통 계산에서 자주 쓰이므로 Wrapping 클래스를 사용하지 않았다.

---
+++ src/main/java/christmas/domain/VisitDate.java
@@ -0,0 +1,21 @@
+package christmas.domain;
+
+import static christmas.constants.ErrorMessage.INVALID_DATE;
+
+public class VisitDate {
+    private static final int START_DATE = 1;
+    private static final int LAST_DATE = 31;
+
+    private final int date;
+
+    public VisitDate(int date) {
+        validate(date);
+        this.date = date;
+    }
+
+    private void validate(int date) {
+        if(date < START_DATE || date > LAST_DATE) {
+            throw new IllegalArgumentException(INVALID_DATE);
+        }
+    }
+}
--- src/main/java/christmas/view/VisitDateRequest.java
+++ src/main/java/christmas/view/VisitDateRequest.java
@@ -2,18 +2,24 @@ package christmas.view;
 
 import static christmas.constants.ErrorMessage.INVALID_DATE;
 
+import christmas.domain.VisitDate;
+
 public class VisitDateRequest {
-    private final Integer date;
+    private final int date;
 
     public VisitDateRequest(String input) {
         this.date = validate(input);
     }
 
-    public Integer validate(String input) {
+    public int validate(String input) {
         try {
             return Integer.valueOf(input.trim());
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INVALID_DATE);
         }
+    }
+
+    public VisitDate convertToValidDate() {
+        return new VisitDate(date);
     }
 }
