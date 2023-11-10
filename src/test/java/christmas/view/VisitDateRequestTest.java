86d6053cf12403a9d9c1c97b00ea7bc4e4c72cf0
jongmee <ra123221@gmail.com>
2023-11-10 21:40:36 +0900

    test(VisitDateRequestTest): 방문 날짜 입력 요청에 대한 테스트하기
    - WHY: 입력 요청 클래스에서는 integer로의 변환에서 발생하는 예외를 처리하므로 해당 예외를 테스트했다.

---
+++ src/test/java/christmas/view/VisitDateRequestTest.java
@@ -0,0 +1,32 @@
+package christmas.view;
+
+import static christmas.constants.ErrorMessage.INVALID_DATE;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class VisitDateRequestTest {
+    @DisplayName("방문 날짜를 입력할 때 정수가 아닌 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "이십오일"})
+    void 정수가아닌_날짜를_입력하면_예외가_발생한다(String 입력) {
+        // when & then
+        assertThatThrownBy(() -> {
+            new VisitDateRequest(입력);
+        }).isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(INVALID_DATE);
+    }
+
+    @DisplayName("방문 날짜를 입력할 때 양옆 공백은 무시된다.")
+    @ParameterizedTest
+    @ValueSource(strings = {" 1 ", "18 ", " 5"})
+    void 양옆공백은_입력에서_무시한다(String 입력) {
+        // when & then
+        assertDoesNotThrow(() -> {
+            new VisitDateRequest(입력);
+        });
+    }
+}
