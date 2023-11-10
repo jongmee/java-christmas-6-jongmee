5363bb15802dd2d11f6e143ea72df41caebf9a0c
jongmee <ra123221@gmail.com>
2023-11-10 15:44:01 +0900

    feat(ErrorMessage): 에러 메세지 상수를 constants 패키지에서 관리하기

---
+++ src/main/java/christmas/constants/ErrorMessage.java
@@ -0,0 +1,9 @@
+package christmas.constants;
+
+public class ErrorMessage {
+    public static final String INVALID_DATE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    public static final String BEVERAGE_ONLY_ERROR = "[ERROR] 음료만 주문할 수 없습니다.";
+    public static final String MAX_ORDER_LIMIT_EXCEEDED = "[ERROR] 메뉴는 한 번에 최대 20개까지 주문할 수 있습니다.";
+    public static final String INVALID_ORDER = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+
+}
