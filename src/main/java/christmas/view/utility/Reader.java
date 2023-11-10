b85bd1b35b016bdaf0313ffe5e8a5fcd04ce43cd
jongmee <ra123221@gmail.com>
2023-11-10 15:44:42 +0900

    feat(Reader, Writer): 입출력을 utility 패키지에서 관리하기

---
+++ src/main/java/christmas/view/utility/Reader.java
@@ -0,0 +1,9 @@
+package christmas.view.utility;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Reader {
+    public static String readline(){
+        return Console.readLine();
+    }
+}
---
+++ src/main/java/christmas/view/utility/Writer.java
@@ -0,0 +1,11 @@
+package christmas.view.utility;
+
+public class Writer {
+    public static void print(Object object) {
+        System.out.print(object);
+    }
+
+    public static void printLine(Object object) {
+        System.out.println(object);
+    }
+}
