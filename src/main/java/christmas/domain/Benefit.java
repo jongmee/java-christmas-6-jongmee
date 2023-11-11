737e3996035dba2c587c678b6985c8e114913ebb
jongmee <ra123221@gmail.com>
2023-11-12 03:52:32 +0900

    feat(Benefit): 방문 날짜로 특별 할인과 크리스마스 할인 적용 여부를 결정하기

--- src/main/java/christmas/domain/Benefit.java
+++ src/main/java/christmas/domain/Benefit.java
@@ -9,7 +9,7 @@ public class Benefit {
     private final Map<Menu, Integer> gifts;
 
     public Benefit(VisitDate date) {
-
+        this.discounts = determineDiscounts(date);
     }
 
     private List<Discount> determineDiscounts(VisitDate date) {
@@ -17,6 +17,26 @@ public class Benefit {
 
         List<Discount> result = new ArrayList<>();
         result.add(Discount.chooseWeekdayOrWeekend(day));
+        if(isAvailableForSpecial(date)) {
+            result.add(Discount.SPECIAL);
+        }
+        if(isAvailableForChristmas(date)) {
+            result.add(Discount.CHRISTMAS);
+        }
+        return result;
+    }
 
+    private static boolean isAvailableForSpecial(VisitDate date) {
+        if(date.isChristmas() || date.calculateDay().equals(Day.SUNDAY)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isAvailableForChristmas(VisitDate date) {
+        if(date.isChristmas() || date.isBeforeChristmas()) {
+            return true;
+        }
+        return false;
     }
 }
