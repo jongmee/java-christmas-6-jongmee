c3903d694324288a5bc796c4063e3569a4fee54f
jongmee <ra123221@gmail.com>
2023-11-13 14:36:04 +0900

    feat(Benefit): 증정 내역 Benefit(혜택) 클래스에 저장하기

--- src/main/java/christmas/domain/Benefit.java
+++ src/main/java/christmas/domain/Benefit.java
@@ -1,18 +1,22 @@
 package christmas.domain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Benefit {
+    private static final int GIFT_CRITERIA = 120_000;
+
     private final List<Discount> discounts;
     private final Map<Menu, Integer> gifts;
 
-    public Benefit(VisitDate date) {
+    public Benefit(final VisitDate date, final Order order) {
         this.discounts = determineDiscounts(date);
+        this.gifts = determineGifts(order);
     }
 
-    private List<Discount> determineDiscounts(VisitDate date) {
+    private List<Discount> determineDiscounts(final VisitDate date) {
         List<Discount> result = new ArrayList<>();
         result.add(Discount.chooseWeekdayOrWeekend(date));
         if(isAvailableForSpecial(date)) {
@@ -24,17 +28,24 @@ public class Benefit {
         return result;
     }
 
-    private static boolean isAvailableForSpecial(VisitDate date) {
+    private boolean isAvailableForSpecial(final VisitDate date) {
         if(date.isChristmas() || date.calculateDay().equals(Day.SUNDAY)) {
             return true;
         }
         return false;
     }
 
-    private static boolean isAvailableForChristmas(VisitDate date) {
+    private boolean isAvailableForChristmas(final VisitDate date) {
         if(date.isChristmas() || date.isBeforeChristmas()) {
             return true;
         }
         return false;
+    }
+
+    private Map<Menu, Integer> determineGifts(final Order order) {
+        if(order.calculateTotalPrice() > GIFT_CRITERIA) {
+            return new HashMap<>(Map.of(Menu.CHAMPAGNE, 1));
+        }
+        return new HashMap<>();
     }
 }
