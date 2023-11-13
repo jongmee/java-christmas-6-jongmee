6572a74058684b21a61d6b7c78e09d7b236cbb1f
jongmee <ra123221@gmail.com>
2023-11-14 02:24:47 +0900

    feat(Benefit): 혜택 도메인 클래스에서 방문날짜와 주문으로 할인 항목별 할인 금액 구하기

--- src/main/java/christmas/domain/Benefit.java
+++ src/main/java/christmas/domain/Benefit.java
@@ -1,45 +1,46 @@
 package christmas.domain;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Benefit {
     private static final int GIFT_CRITERIA = 120_000;
 
-    private final List<Discount> discounts;
+    private final Map<Discount, Integer> discounts;
     private final Map<Menu, Integer> gifts;
 
     public Benefit(final VisitDate date, final Order order) {
-        this.discounts = determineDiscounts(date);
+        this.discounts = determineDiscounts(date, order);
         this.gifts = determineGifts(order);
     }
 
-    private List<Discount> determineDiscounts(final VisitDate date) {
-        List<Discount> result = new ArrayList<>();
-        result.add(Discount.chooseWeekdayOrWeekend(date));
-        if(isAvailableForSpecial(date)) {
-            result.add(Discount.SPECIAL);
-        }
-        if(isAvailableForChristmas(date)) {
-            result.add(Discount.CHRISTMAS);
-        }
+    private Map<Discount, Integer> determineDiscounts(final VisitDate date, final Order order) {
+        Map<Discount, Integer> result = new HashMap<>();
+        result.putAll(determineWeekDiscount(date, order));
+        result.putAll(determineSpecialDiscount(date));
+        result.putAll(determineChristmasDiscount(date));
         return result;
     }
 
-    private boolean isAvailableForSpecial(final VisitDate date) {
-        if(date.isChristmas() || date.calculateDay().equals(Day.SUNDAY)) {
-            return true;
+    private Map<Discount, Integer> determineWeekDiscount(final VisitDate date, final Order order) {
+        if(date.calculateDay().isWeekend()) {
+            return Discount.WEEKEND.calculateWeekDiscount(order);
         }
-        return false;
+        return Discount.WEEKDAY.calculateWeekDiscount(order);
     }
 
-    private boolean isAvailableForChristmas(final VisitDate date) {
-        if(date.isChristmas() || date.isBeforeChristmas()) {
-            return true;
+    private Map<Discount, Integer> determineSpecialDiscount(final VisitDate date) {
+        if(date.isChristmas() || date.calculateDay().equals(Day.SUNDAY)) {
+            return Discount.SPECIAL.calculateSpecialDiscount();
         }
-        return false;
+        return new HashMap<>();
+    }
+
+    private Map<Discount, Integer> determineChristmasDiscount(final VisitDate date) {
+        if(date.isChristmas() || date.isBeforeChristmas()) {
+            return Discount.CHRISTMAS.calculateChristmasDiscount(date);
+        }
+        return new HashMap<>();
     }
 
     private Map<Menu, Integer> determineGifts(final Order order) {
@@ -47,5 +48,9 @@ public class Benefit {
             return new HashMap<>(Map.of(Menu.CHAMPAGNE, 1));
         }
         return new HashMap<>();
+    }
+
+    public Map<Discount, Integer> getDiscounts() {
+        return discounts;
     }
 }
