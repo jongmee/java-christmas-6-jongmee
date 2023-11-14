1d583f14e281485f8ceb767831e134def1ed4d4a
jongmee <ra123221@gmail.com>
2023-11-14 21:13:53 +0900

    feat(Application): 메인 함수에서 프로그램 실행하기

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,10 @@
 package christmas;
 
+import christmas.config.AppConfig;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppConfig config = new AppConfig();
+        config.promotionController().run();
     }
 }
