4cad3922f68a2719f27b663d8c3b49283e50d842
jongmee <ra123221@gmail.com>
2023-11-14 13:39:57 +0900

    feat(OutputView): 방문날짜와 주문, 총 주문 금액을 출력하기

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,6 +1,9 @@
 package christmas.view;
 
+import christmas.view.utility.Writer;
+
 public class OutputView {
+    private static final String BENEFIT_MESSAGE = "12월 %s일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     private static final String ORDER_MENU_HEADER = "<주문 메뉴>";
     private static final String ORDER_AMOUNT_HEADER = "<할인 전 총주문 금액>";
     private static final String GIFT_MENU_HEADER = "<증정 메뉴>";
@@ -8,4 +11,17 @@ public class OutputView {
     private static final String BENEFIT_AMOUNT_HEADER = "<총혜택 금액>";
     private static final String TOTAL_PAYMENT_HEADER = "<할인 후 예상 결제 금액>";
     private static final String BADGE_HEADER = "<12월 이벤트 배지>";
+
+    public static void alertBenefit(String visitDate) {
+        Writer.printLine(String.format(BENEFIT_MESSAGE, visitDate));
+    }
+
+    public static void alertOrder(OrderResponse response, int orderAmount) {
+        Writer.printLine(ORDER_MENU_HEADER);
+        response.getMessage().stream()
+            .forEach(Writer::printLine);
+
+        Writer.printLine(ORDER_AMOUNT_HEADER);
+        Writer.printLine(orderAmount);
+    }
 }
